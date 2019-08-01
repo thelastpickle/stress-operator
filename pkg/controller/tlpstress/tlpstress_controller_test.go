f0948890bb87d031de6a0f68a7573b1f8a61e82d
John Sanda <john.sanda@gmail.com>
2019-08-01 16:20:26 -0400

    add test to verify defaults are not set when values are supplied in spec

--- pkg/controller/tlpstress/tlpstress_controller_test.go
+++ pkg/controller/tlpstress/tlpstress_controller_test.go
@@ -73,3 +73,71 @@ func TestTLPStressControllerDefaultsSet(t *testing.T) {
 		t.Errorf("ImagePullPolicy (%s) is not the expected value (%s)", instance.Spec.ImagePullPolicy, corev1.PullAlways)
 	}
 }
+
+func TestTLPStressControllerDefaultsNotSet(t *testing.T) {
+	var (
+		name = "tlpstress-controller"
+		namespace = "tlpstress"
+	)
+
+	tlpStress := &v1alpha1.TLPStress{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+			Namespace: namespace,
+		},
+		Spec: v1alpha1.TLPStressSpec{
+			CassandraService: "cassandra-service",
+			Workload: "BasicTimeSeries",
+			Image: "jsanda/tlp-stress:test",
+			ImagePullPolicy: corev1.PullIfNotPresent,
+		},
+	}
+
+	objs := []runtime.Object{ tlpStress }
+
+	s := scheme.Scheme
+	s.AddKnownTypes(v1alpha1.SchemeGroupVersion, tlpStress)
+
+	cl := fake.NewFakeClient(objs...)
+
+	r := &ReconcileTLPStress{client: cl, scheme: s}
+
+	req := reconcile.Request{
+		NamespacedName: types.NamespacedName{
+			Name: name,
+			Namespace: namespace,
+		},
+	}
+
+	res, err := r.Reconcile(req)
+	if err != nil {
+		t.Fatalf("reconcile: (%v)", err)
+	}
+	// Check the result of reconciliation to make sure it has the desired state.
+	if !res.Requeue {
+		t.Error("reconcile did not requeue request as expected")
+	}
+
+	// make sure defaults are assigned
+	instance := &v1alpha1.TLPStress{}
+	err = r.client.Get(context.TODO(), req.NamespacedName, instance)
+	if err != nil {
+		t.Fatalf("get TLPStress: (%v)", err)
+	}
+
+	if instance.Spec.Workload != tlpStress.Spec.Workload {
+		t.Errorf("Workload (%s) is not the expected value (%s)", instance.Spec.Workload, tlpStress.Spec.Workload)
+	}
+
+	if instance.Spec.Image != tlpStress.Spec.Image {
+		t.Errorf("Image (%s) is not the expected value (%s)", instance.Spec.Image, tlpStress.Spec.Image)
+	}
+
+	if instance.Spec.ImagePullPolicy != tlpStress.Spec.ImagePullPolicy {
+		t.Errorf("ImagePullPolicy (%s) is not the expected value (%s)", instance.Spec.ImagePullPolicy, tlpStress.Spec.ImagePullPolicy)
+	}
+}
+
+//func TestTLPStressControllerJobCreate(t *testing.T) {
+//
+//}
