1adab2bbdb62657f97163cdcc7a13b8d89b10263
John Sanda <john.sanda@gmail.com>
2019-08-27 00:21:07 -0400

    Start adding help functions for building a cassandra cluster
    This is based on
    https://kubernetes.io/docs/tutorials/stateful-application/cassandra/.

--- test/e2e/tlpstress_test.go
+++ test/e2e/tlpstress_test.go
@@ -3,10 +3,14 @@ package e2e
 import (
 	"github.com/jsanda/tlp-stress-operator/pkg/apis"
 	"github.com/operator-framework/operator-sdk/pkg/test/e2eutil"
+	"k8s.io/kubernetes/pkg/apis/apps"
 	"testing"
 	"time"
 	tlp "github.com/jsanda/tlp-stress-operator/pkg/apis/thelastpickle/v1alpha1"
 	framework "github.com/operator-framework/operator-sdk/pkg/test"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	appsv1 "k8s.io/api/apps/v1"
 )
 
 var (
@@ -43,11 +47,70 @@ func runOneTLPStress(t *testing.T) {
 	}
 	// get global framework variables
 	f := framework.Global
-	// wait for memcached-operator to be ready
+	// wait for tlp-stress-operator to be ready
 	err = e2eutil.WaitForDeployment(t, f.KubeClient, namespace, "tlp-stress-operator", 1, retryInterval, timeout)
 	if err != nil {
 		t.Fatal(err)
 	}
+}
 
+func createCassandraCluster(t *testing.T, f *framework.Framework) {
+	createHeadlessService(t, f)
+	createCassandraCluster(t, f)
+}
 
+func createHeadlessService(t *testing.T, f *framework.Framework) {
+	_, err := f.KubeClient.CoreV1().Services(f.Namespace).Create(&corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "cassandra",
+			Namespace: f.Namespace,
+			Labels: map[string]string{
+				"app": "cassandra",
+			},
+		},
+		Spec: corev1.ServiceSpec{
+			ClusterIP: "None",
+			Ports: []corev1.ServicePort{
+				{Port: 9042},
+			},
+			Selector: map[string]string{
+				"app": "cassandra",
+			},
+		},
+	})
+	if err != nil {
+		t.Fatalf("Failed to create Cassandra service: %s", err)
+	}
+}
+
+func createStatefulSet(t *testing.T, f *framework.Framework) {
+	_, err := f.KubeClient.AppsV1().StatefulSets(f.Namespace).Create(&appsv1.StatefulSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "cassandra",
+			Namespace: f.Namespace,
+			Labels: map[string]string{
+				"app": "cassandra",
+			},
+		},
+		Spec: appsv1.StatefulSetSpec{
+			ServiceName: "cassandra",
+			Replicas: int32Ptr(1),
+			Selector: &metav1.LabelSelector{
+				MatchExpressions: []metav1.LabelSelectorRequirement{
+					{
+						Key:      "app",
+						Operator: metav1.LabelSelectorOpIn,
+						Values:   []string{"cassandra"},
+					},
+				},
+			},
+		},
+	})
+	if err != nil {
+		t.Fatalf("Failed to create Cassandra statefulset: %s", err)
+	}
+}
+
+func int32Ptr(n int32) *int32 {
+	return &n
 }
